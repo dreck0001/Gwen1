34f1fb0b49f2ea9bc840dd11cffbed24694f954a
Denis Ansah <denis.ansah@yahoo.com>
2024-05-24 14:56:11 -0400

    Initial Commit

---
+++ Gwen/GwenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GwenApp.swift
+//  Gwen
+//
+//  Created by Denis on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GwenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
